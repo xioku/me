7635a702cdb4c8eb439ee23e9d22de48e83a80e9
vipx.o <6045698+vipx@users.noreply.github.com>
2021-05-13 14:41:33 +0800

    Update mixin.user.js

--- mixin.user.js
+++ mixin.user.js
@@ -4,19 +4,15 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *://*/*
-// @match        *://*/*?*
+// @match        *://ctjsoft.mrcrm.com/*
+// @match        *://ctjsoft.mrcrm.com/*?*
 // @require      https://unpkg.com/ajax-hook@2.0.3/dist/ajaxhook.min.js
-// @run-at       document-end
-// @grant        unsafeWindow
-// @grant        GM_xmlhttpRequest
-// @grant        GM_getValue
-// @grant        GM_setClipboard
-// @grant        GM_setValue
+// @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    console.log("脚本生效中...");
     function replacePV(url,name,value) {
         var oUrl = url;
         var re=eval('/('+name+'=)([^&]*)/gi');
@@ -29,15 +25,21 @@
             var url = config.url;
             if (url.indexOf('location.action') >= 0) {
                 var p = config.body;
-                p = replacePV(p,'longitude','113.68627421602186')
-                p = replacePV(p,'latitude','34.78460976085749')
-                console.log(p);
+                p = replacePV(p,'longitude','113.68627421602186');
+                p = replacePV(p,'latitude','34.78460976085749');
+                p = replacePV(p,'accuracy',((Math.random()*50+50)+'').substring(0,5));
                 config.body = p;
             }
             handler.next(config);
         },
         //请求成功后进入
         onResponse: (response, handler) => {
+            var url = response.config.url;
+            if (url.indexOf('location.action') >= 0) {
+                var loc = JSON.parse(response.response);
+                loc.name = '河南省郑州市金水区经三路 在财富广场附近';
+                response.response = JSON.stringify(loc);
+            }
             handler.next(response);
         }
     });
